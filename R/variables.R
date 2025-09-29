7499d9cc094c75631dc5d42206a9ec35c16ef84c
Andrew Pulsipher <pulsipher.a@gmail.com>
2025-09-29 16:20:50 -0600

    Add definition for support type component of respiratory failure severity and NA report

--- R/variables.R
+++ R/variables.R
@@ -77,6 +77,57 @@ calc_ddimer_0 <- function(daily_ddimer_8a_0, daily_ddimer_nc_0) {
 }
 
 
+#' Calculate the support type component of the systematic DAG variable for respiratory failure severity on Day 0
+#'
+#' `calc_resp_support_type_0` calculates the respiratory support type
+#' component of the systematic DAG variable for respiratory failure severity
+#' from the data.
+#'
+#' @param daily_resp_8a_0_code Integer vector. The `daily_resp_8a_0_code` column from the data.
+#' @param dailysofa_perf_0 Character vector. The `dailysofa_perf_0` column from the data.
+#' @param daily_standard_flow_8a_0 Numeric vector. The `daily_standard_flow_8a_0` column from the data.
+#' @param daily_hfnc_fi02_8a_0 Numeric vector. The `daily_hfnc_fi02_8a_0` column from the data.
+#' @param daily_niv_fi02_8a_0 Numeric vector. The `daily_niv_fi02_8a_0` column from the data.
+#' @param daily_imv_fio2_8a_0 Numeric vector. The `daily_imv_fio2_8a_0` column from the data.
+#' @param daily_epap_8a_0 Numeric vector. The `daily_epap_8a_0` column from the data.
+#'
+#' @returns A vector with values:
+#' - 1 = No respiratory support on day 0
+#' - 2 = Standard flow
+#' - 3 = HFNC
+#' - 4 = NIV
+#' - 5 = IMV with PEEP < 12 but no ECMO
+#' - 6 = IMV with PEEP ≥ 12 or ECMO
+#' @export
+calc_resp_support_type_0 <- function(
+  daily_resp_8a_0_code,
+  dailysofa_perf_0,
+  daily_standard_flow_8a_0,
+  daily_hfnc_fi02_8a_0,
+  daily_niv_fi02_8a_0,
+  daily_imv_fio2_8a_0,
+  daily_epap_8a_0
+) {
+
+  dplyr::case_when(
+    # ECMO
+    daily_resp_8a_0_code %in% c(1, 2) ~ 6,
+    # IMV with PEEP >= 12 (same code as ECMO)
+    daily_resp_8a_0_code == 3 & !is.na(daily_imv_fio2_8a_0) & daily_epap_8a_0 >= 12 ~ 6,
+    # IMV with PEEP < 12
+    daily_resp_8a_0_code == 3 & !is.na(daily_imv_fio2_8a_0) & daily_epap_8a_0 < 12 ~ 5,
+    # NIV
+    (daily_resp_8a_0_code == 4) & !is.na(daily_niv_fi02_8a_0) ~ 4,
+    # HFNC
+    (daily_resp_8a_0_code == 5) & !is.na(daily_hfnc_fi02_8a_0) ~ 3,
+    # Standard flow
+    (daily_resp_8a_0_code == 6) & !is.na(daily_standard_flow_8a_0) ~ 2,
+    # No respiratory support on day 0 or not applicable
+    (daily_resp_8a_0_code == 7 | dailysofa_perf_0 == 'Not Available') ~ 1
+  )
+}
+
+
 #' Derive systematic respiratory failure severity variables
 #'
 #' `derive_systematic_respiratory_failure_severity_0` derives the
