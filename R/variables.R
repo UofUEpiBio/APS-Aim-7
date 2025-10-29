b0b747479bf3caa613063a6d2cde9a363c05798d
Andrew Pulsipher <pulsipher.a@gmail.com>
2025-10-28 20:59:16 -0600

    Add Hyperglycemia variables

--- R/variables.R
+++ R/variables.R
@@ -529,3 +529,68 @@ calc_sys_chron_immunocomp_0 <- function(
       is_checked(m_immunosup_conditions___88) ~ 1
   )
 }
+
+
+#' Calculate the systematic DAG variable 1 for hyperglycemia on Day 0
+#'
+#' `calc_sys_hyperglyc_hist_0` calculates the systematic DAG variable 1 for
+#' hyperglycemia from the data.
+#'
+#' @param m_endo_conditions___1 Character vector. The `m_endo_conditions___1` column
+#' from the data.
+#'
+#' @returns A vector with values:
+#' - 0 = No diabetes medical history
+#' - 1 = Diabetes medical history present
+#' @export
+calc_sys_hyperglyc_hist_0 <- function(m_endo_conditions___1) {
+  dplyr::case_when(
+    is_unchecked(m_endo_conditions___1) ~ 0,
+    is_checked(m_endo_conditions___1) ~ 1
+  )
+}
+
+
+#' Calculate the systematic DAG variable 2 for hyperglycemia on Day 0
+#'
+#' `calc_sys_hyperglyc_gluc_0` calculates the systematic DAG variable 2 for
+#' hyperglycemia from the data.
+#'
+#' @param daily_gluc_8a_0 Numeric vector. The `daily_gluc_8a_0` column from the data.
+#' @param daily_gluc_nc_0 Character vector. The `daily_gluc_nc_0` column from the data.
+#'
+#' @returns A numeric vector representing the glucose level on day 0 based on
+#' available data:
+#' - Glucose value if measured
+#' - -1 if glucose not collected
+#' @export
+calc_sys_hyperglyc_gluc_0 <- function(
+  daily_gluc_8a_0,
+  daily_gluc_nc_0
+) {
+  dplyr::case_when(
+    !is.na(daily_gluc_8a_0) ~ daily_gluc_8a_0,
+    daily_gluc_nc_0 == 'Not Collected' ~ -1
+  )
+}
+
+#' Calculate the streamlined DAG variable for hyperglycemia on Day 0
+#'
+#' `calc_str_hyperglyc_hist_0` calculates the streamlined DAG variable for
+#' hyperglycemia from the data.
+#'
+#' @inheritParams calc_sys_hyperglyc_gluc_0
+#'
+#' @returns A vector with values:
+#' - 0 = No hyperglycemia on day 0 (glucose ≤ 250 or not collected)
+#' - 1 = Hyperglycemia present (glucose > 250)
+calc_str_hyperglyc_hist_0 <- function(
+  daily_gluc_8a_0,
+  daily_gluc_nc_0
+) {
+  dplyr::case_when(
+    daily_gluc_8a_0 > 250 ~ 1,
+    is.na(daily_gluc_8a_0) | (daily_gluc_8a_0 <= 250) ~ 0,
+    daily_gluc_nc_0 == 'Not Collected' ~ 0
+  )
+}
