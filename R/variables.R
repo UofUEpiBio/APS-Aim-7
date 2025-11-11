080431fc4f2df66e0393eba04aea08d42010461b
Andrew Pulsipher <pulsipher.a@gmail.com>
2025-11-10 21:36:28 -0700

    Add streamlined DAG for age

--- R/variables.R
+++ R/variables.R
@@ -412,3 +412,20 @@ calc_str_hyperglyc_hist_0 <- function(
     daily_gluc_nc_0 == 'Not Collected' ~ 0
   )
 }
+
+
+#' Calculate the streamlined DAG variable for age on Day 0
+#'
+#' `calc_age_0` calculates the streamlined DAG variable for age from the data.
+#'
+#' @param age Numeric vector. The `age` column from the data.
+#'
+#' @returns A numeric vector representing age on day 0.
+#' @export
+calc_age_0 <- function(
+  age
+) {
+  dplyr::case_when(
+    !is.na(age) & age >= 0 ~ age
+  )
+}
