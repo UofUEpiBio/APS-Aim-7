d162d20efa3acd5a558c7593bc3c8c5729d410dc
Andrew Pulsipher <pulsipher.a@gmail.com>
2025-11-19 21:50:34 -0700

    Rename rheumatologic disease to steroid responsive comorbidity and udpate logic for streamlined component

--- R/variables.R
+++ R/variables.R
@@ -88,14 +88,14 @@ calc_str_chron_steroid_highdose_0 <- function(
 
 
 ## -----------------------------------------------------------------------------
-## Rheumatologic Disease (Systematic & Streamlined DAG)
+## Steroid Responsive Comorbidity (Systematic & Streamlined DAG)
 ## -----------------------------------------------------------------------------
 
 
-#' Calculate the systematic DAG variable for rheumatologic disease on Day 0
+#' Calculate the systematic DAG variable for steroid responsive comorbidity on Day 0
 #'
-#' `calc_sys_rheumdis_0` calculates the systematic DAG variable for
-#' rheumatologic disease from the data.
+#' `calc_sys_steroid_comorb_0` calculates the systematic DAG variable for
+#' steroid responsive comorbidity from the data.
 #'
 #' @param mhrheumd Character vector. The `mhrheumd` column from the data.
 #' @param m_rheum_conditions___1 Character vector. The `m_rheum_conditions___1` column
@@ -121,11 +121,11 @@ calc_str_chron_steroid_highdose_0 <- function(
 #' from the data.
 #'
 #' @returns A vector with values:
-#' - 0 = No rheumatologic disease
-#' - 1 = Rheumatologic disease present
+#' - 0 = No steroid responsive comorbidity
+#' - 1 = Steroid responsive comorbidity present
 #' - 99 = Unknown
 #' @export
-calc_sys_rheumdis_0 <- function(
+calc_sys_steroid_comorb_0 <- function(
   mhrheumd,
   m_rheum_conditions___1,
   m_rheum_conditions___2,
@@ -167,21 +167,22 @@ calc_sys_rheumdis_0 <- function(
       ) |
       (m_pulmonary == "Yes" &
       is_checked(m_pulm_conditions___6)) ~ 1,
+
       mhrheumd == "Unknown" | m_pulmonary == "Unknown" ~ 99
   )
 }
 
 
-#' Calculate the streamlined DAG variable for rheumatologic disease on Day 0
+#' Calculate the streamlined DAG variable for steroid responsive comorbidity on Day 0
 #'
-#' `calc_str_rheumdis_0` calculates the streamlined DAG variable for
-#' rheumatologic disease from the data.
+#' `calc_str_steroid_comorb_0` calculates the streamlined DAG variable for
+#' steroid responsive comorbidity from the data.
 #'
-#' @inheritParams calc_sys_rheumdis_0
+#' @inheritParams calc_sys_steroid_comorb_0
 #'
-#' @inherits calc_sys_rheumdis_0 returns
+#' @inherits calc_sys_steroid_comorb_0 returns
 #' @export
-calc_str_rheumdis_0 <- function(
+calc_str_steroid_comorb_0 <- function(
   mhrheumd,
   m_rheum_conditions___1,
   m_rheum_conditions___2,
@@ -190,11 +191,13 @@ calc_str_rheumdis_0 <- function(
   m_rheum_conditions___5,
   m_rheum_conditions___6,
   m_rheum_conditions___7,
-  m_rheum_conditions___8,
-  m_rheum_conditions___88
+  m_rheum_conditions___88,
+  m_immunosuppression,
+  m_immunosup_conditions___4,
+  mhccster
 ) {
   dplyr::case_when(
-    mhrheumd == "No" &
+    (mhrheumd == "No" &
       is_unchecked(m_rheum_conditions___1) &
       is_unchecked(m_rheum_conditions___2) &
       is_unchecked(m_rheum_conditions___3) &
@@ -202,10 +205,12 @@ calc_str_rheumdis_0 <- function(
       is_unchecked(m_rheum_conditions___5) &
       is_unchecked(m_rheum_conditions___6) &
       is_unchecked(m_rheum_conditions___7) &
-      is_unchecked(m_rheum_conditions___8) &
-      is_unchecked(m_rheum_conditions___88) ~ 0,
+      is_unchecked(m_rheum_conditions___88)) |
+    (m_immunosuppression %in% c("Yes", "No") &
+      is_unchecked(m_immunosup_conditions___4) &
+      mhccster == "No") ~ 0,
 
-    mhrheumd == "Yes" & (
+    (mhrheumd == "Yes" & (
       is_checked(m_rheum_conditions___1) |
       is_checked(m_rheum_conditions___2) |
       is_checked(m_rheum_conditions___3) |
@@ -213,10 +218,13 @@ calc_str_rheumdis_0 <- function(
       is_checked(m_rheum_conditions___5) |
       is_checked(m_rheum_conditions___6) |
       is_checked(m_rheum_conditions___7) |
-      is_checked(m_rheum_conditions___8) |
-      is_checked(m_rheum_conditions___88)
-      ) ~ 1,
-    mhrheumd == "Unknown" ~ 99
+      is_checked(m_rheum_conditions___88))
+      ) &
+    ((m_immunosuppression == "Yes" &
+      is_checked(m_immunosup_conditions___4)) |
+      mhccster == "Yes") ~ 1,
+
+    is_unknown(mhrheumd) | is_unknown(m_immunosuppression) | is_unknown(mhccster) ~ 99
   )
 }
 
@@ -281,7 +289,7 @@ calc_sys_active_covid19_0 <- function(
 
 
 ## -----------------------------------------------------------------------------
-## Chronic Immunocompromise (Systematic & Streamlined DAG)
+## Chronic Immunocompromise (Systematic DAG)
 ## -----------------------------------------------------------------------------
 
 
