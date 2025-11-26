650f2060ceacedb8eadcc92cbcaa03ad498e75a7
Andrew Pulsipher <pulsipher.a@gmail.com>
2025-11-25 21:37:12 -0700

    Update Inflammatory Profile to look back up to 2 days if missing Day 0

--- R/queried-vars.R
+++ R/queried-vars.R
@@ -32,7 +32,8 @@ calc_sys_nmblockade_0 <- function(daily_paralysis_0, trx_0) {
 #'
 #' `calc_str_inflamprofile_0` calculates the streamlined DAG variable for
 #' inflammatory profile from the data. This variable represents
-#' CRP level on admission day.
+#' CRP level on admission day. If the Day 0 value is missing, it looks back up to
+#' 2 days to find a valid value.
 #'
 #' @param daily_crp_8a_0 Numeric vector. The `daily_crp_8a_0` column from the data.
 #' @param daily_crp_nc_0 Character vector. The `daily_crp_nc_0` column from the data.
@@ -42,44 +43,130 @@ calc_sys_nmblockade_0 <- function(daily_paralysis_0, trx_0) {
 #' - 1 = Day 0 CRP checked and < 15
 #' - 2 = Day 0 CRP checked and ≥ 15
 #' @export
-calc_str_inflamprofile_0 <- function(daily_crp_8a_0, daily_crp_nc_0) {
-  calc_crp_0(daily_crp_8a_0, daily_crp_nc_0)
+calc_str_inflamprofile_0 <- function(
+  daily_crp_8a_0,
+  daily_crp_nc_0,
+  daily_crp_8a_m1,
+  daily_crp_nc_m1,
+  daily_crp_8a_m2,
+  daily_crp_nc_m2
+  ) {
+  calc_crp_0(
+    daily_crp_8a_0,
+    daily_crp_nc_0,
+    daily_crp_8a_m1,
+    daily_crp_nc_m1,
+    daily_crp_8a_m2,
+    daily_crp_nc_m2
+  )
 }
 
 
 # Calculate the CRP component of the inflammatory profile variable
-calc_crp_0 <- function(daily_crp_8a_0, daily_crp_nc_0) {
+calc_crp_0 <- function(
+  daily_crp_8a_0,
+  daily_crp_nc_0,
+  daily_crp_8a_m1,
+  daily_crp_nc_m1,
+  daily_crp_8a_m2,
+  daily_crp_nc_m2
+  ) {
   dplyr::case_when(
-    daily_crp_8a_0 >= 15 ~ 2,
-    daily_crp_8a_0 < 15 ~ 1,
-    daily_crp_nc_0 == 'Not Collected' ~ 0
+    # Day 0 CRP >= 15 (if missing, look back up to 2 days)
+    daily_crp_8a_0 >= 15 |
+      (is.na(daily_crp_8a_0) & daily_crp_8a_m1 >= 15) |
+      (is.na(daily_crp_8a_0) & is.na(daily_crp_8a_m1) & daily_crp_8a_m2 >= 15) ~ 2,
+
+    # Day 0 CRP >= 15 (if missing, look back up to 2 days)
+    daily_crp_8a_0 < 15 |
+      (is.na(daily_crp_8a_0) & daily_crp_8a_m1 < 15) |
+      (is.na(daily_crp_8a_0) & is.na(daily_crp_8a_m1) & daily_crp_8a_m2 < 15) ~ 1,
+
+    # Day 0 CRP not collected (if missing, look back up to 2 days)
+    daily_crp_nc_0 == 'Not Collected' |
+      daily_crp_nc_m1 == 'Not Collected' |
+      daily_crp_nc_m2 == 'Not Collected' ~ 0
   )
 }
 
 # Calculate the Ferritin component of the inflammatory profile variable
-calc_ferritin_0 <- function(daily_ferritin_8a_0, daily_ferritin_nc_0) {
+calc_ferritin_0 <- function(
+  daily_ferritin_8a_0,
+  daily_ferritin_nc_0,
+  daily_ferritin_8a_m1,
+  daily_ferritin_nc_m1,
+  daily_ferritin_8a_m2,
+  daily_ferritin_nc_m2
+  ) {
   dplyr::case_when(
-    daily_ferritin_8a_0 >= 700 ~ 2,
-    daily_ferritin_8a_0 < 700 ~ 1,
-    daily_ferritin_nc_0 == 'Not Collected' ~ 0
+    # Day 0 Ferritin >= 700 (if missing, look back up to 2 days)
+    daily_ferritin_8a_0 >= 700 |
+      (is.na(daily_ferritin_8a_0) & daily_ferritin_8a_m1 >= 700) |
+      (is.na(daily_ferritin_8a_0) & is.na(daily_ferritin_8a_m1) & daily_ferritin_8a_m2 >= 700) ~ 2,
+
+    # Day 0 Ferritin < 700 (if missing, look back up to 2 days)
+    daily_ferritin_8a_0 < 700 |
+      (is.na(daily_ferritin_8a_0) & daily_ferritin_8a_m1 < 700) |
+      (is.na(daily_ferritin_8a_0) & is.na(daily_ferritin_8a_m1) & daily_ferritin_8a_m2 < 700) ~ 1,
+
+    # Day 0 Ferritin not collected (if missing, look back up to 2 days)
+    daily_ferritin_nc_0 == 'Not Collected' |
+      daily_ferritin_nc_m1 == 'Not Collected' |
+      daily_ferritin_nc_m2 == 'Not Collected' ~ 0
   )
 }
 
 # Calculate the Fibrinogen component of the inflammatory profile variable
-calc_fibrinogen_0 <- function(daily_fibrinogen_8a_0, daily_fibrinogen_nc_0) {
+calc_fibrinogen_0 <- function(
+  daily_fibrinogen_8a_0,
+  daily_fibrinogen_nc_0,
+  daily_fibrinogen_8a_m1,
+  daily_fibrinogen_nc_m1,
+  daily_fibrinogen_8a_m2,
+  daily_fibrinogen_nc_m2
+  ) {
   dplyr::case_when(
-    daily_fibrinogen_8a_0 >= 150 ~ 2,
-    daily_fibrinogen_8a_0 < 150 ~ 1,
-    daily_fibrinogen_nc_0 == 'Not Collected' ~ 0
+    # Day 0 Fibrinogen >= 150 (if missing, look back up to 2 days)
+    daily_fibrinogen_8a_0 >= 150 |
+      (is.na(daily_fibrinogen_8a_0) & daily_fibrinogen_8a_m1 >= 150) |
+      (is.na(daily_fibrinogen_8a_0) & is.na(daily_fibrinogen_8a_m1) & daily_fibrinogen_8a_m2 >= 150) ~ 2,
+
+    # Day 0 Fibrinogen < 150 (if missing, look back up to 2 days)
+    daily_fibrinogen_8a_0 < 150 |
+      (is.na(daily_fibrinogen_8a_0) & daily_fibrinogen_8a_m1 < 150) |
+      (is.na(daily_fibrinogen_8a_0) & is.na(daily_fibrinogen_8a_m1) & daily_fibrinogen_8a_m2 < 150) ~ 1,
+
+    # Day 0 Fibrinogen not collected (if missing, look back up to 2 days)
+    daily_fibrinogen_nc_0 == 'Not Collected' |
+      daily_fibrinogen_nc_m1 == 'Not Collected' |
+      daily_fibrinogen_nc_m2 == 'Not Collected' ~ 0
   )
 }
 
 # Calculate the D-dimer component of the inflammatory profile variable
-calc_ddimer_0 <- function(daily_ddimer_8a_0, daily_ddimer_nc_0) {
+calc_ddimer_0 <- function(
+  daily_ddimer_8a_0,
+  daily_ddimer_nc_0,
+  daily_ddimer_8a_m1,
+  daily_ddimer_nc_m1,
+  daily_ddimer_8a_m2,
+  daily_ddimer_nc_m2
+  ) {
   dplyr::case_when(
-    daily_ddimer_8a_0 >= 1500 ~ 2,
-    daily_ddimer_8a_0 < 1500 ~ 1,
-    daily_ddimer_nc_0 == 'Not Collected' ~ 0
+    # Day 0 D-dimer >= 1500 (if missing, look back up to 2 days)
+    daily_ddimer_8a_0 >= 1500 |
+      (is.na(daily_ddimer_8a_0) & daily_ddimer_8a_m1 >= 1500) |
+      (is.na(daily_ddimer_8a_0) & is.na(daily_ddimer_8a_m1) & daily_ddimer_8a_m2 >= 1500) ~ 2,
+
+    # Day 0 D-dimer < 1500 (if missing, look back up to 2 days)
+    daily_ddimer_8a_0 < 1500 |
+      (is.na(daily_ddimer_8a_0) & daily_ddimer_8a_m1 < 1500) |
+      (is.na(daily_ddimer_8a_0) & is.na(daily_ddimer_8a_m1) & daily_ddimer_8a_m2 < 1500) ~ 1,
+
+    # Day 0 D-dimer not collected (if missing, look back up to 2 days)
+    daily_ddimer_nc_0 == 'Not Collected' |
+      daily_ddimer_nc_m1 == 'Not Collected' |
+      daily_ddimer_nc_m2 == 'Not Collected' ~ 0
   )
 }
 
