d88dab538277677f0ea7eddae374a7dc95496f2b
Andrew Pulsipher <pulsipher.a@gmail.com>
2025-12-01 23:31:29 -0700

    Add Active COVID-19 variable summary and calculation

--- R/variables.R
+++ R/variables.R
@@ -3,20 +3,22 @@
 ## -----------------------------------------------------------------------------
 
 
-#' WIP
-calc_sys_active_covid19_0 <- function(
-  pathogen_date,
-  resp_pathogen_code,
-  cxpos_code
-) {
+#' Calculate the systematic DAG variable for active COVID-19 infection on Day 0
+#'
+#' `calc_sys_active_covid19_0` calculates the systematic DAG variable for
+#' active COVID-19 infection from the data.
+#'
+#' @param has_pos_covid_0 Numeric vector. Indicator (1) if patient had a positive
+#' COVID-19 test on or before enrollment, NA otherwise.
+#'
+#' @returns A vector with values:
+#' - 0 = No active COVID-19 (not positive on or before Day 0, or not tested)
+#' - 1 = Active COVID-19 (tested positive on or before Day 0)
+#' @export
+calc_sys_active_covid19_0 <- function(has_pos_covid_0) {
   dplyr::case_when(
-    !is.na(pathogen_date) &
-      resp_pathogen_code == 6 &
-      cxpos_code == 3 ~ 1,
-
-    is.na(pathogen_date) |
-      resp_pathogen_code != 6 |
-      cxpos_code != 3 ~ 0
+    has_pos_covid_0 == 1 ~ 1,
+    is.na(has_pos_covid_0) ~ 0
   )
 }
 
