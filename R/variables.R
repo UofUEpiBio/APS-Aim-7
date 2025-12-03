7bbe871b22e30b198fc535b84797941e7f389322
Andrew Pulsipher <pulsipher.a@gmail.com>
2025-12-02 20:53:04 -0700

    Add Active Influenza infection variable

--- R/variables.R
+++ R/variables.R
@@ -24,6 +24,31 @@ calc_sys_active_covid19_0 <- function(has_pos_covid_0) {
 
 
 ## -----------------------------------------------------------------------------
+## Active Influenza Infection (Systematic & Streamlined DAG)
+## -----------------------------------------------------------------------------
+
+
+#' Calculate the systematic DAG variable for active influenza infection on Day 0
+#'
+#' `calc_sys_active_influenza_0` calculates the systematic DAG variable for
+#' active influenza infection from the data.
+#'
+#' @param has_pos_influenza_0 Numeric vector. Indicator (1) if patient had a positive
+#' influenza test on or before enrollment, NA otherwise.
+#'
+#' @returns A vector with values:
+#' - 0 = No active influenza (not positive on or before Day 0, or not tested)
+#' - 1 = Active influenza (tested positive on or before Day 0)
+#' @export
+calc_sys_active_influenza_0 <- function(has_pos_influenza_0) {
+  dplyr::case_when(
+    has_pos_influenza_0 == 1 ~ 1,
+    is.na(has_pos_influenza_0) ~ 0
+  )
+}
+
+
+## -----------------------------------------------------------------------------
 ## Baseline Performance Status (Systematic DAG)
 ## -----------------------------------------------------------------------------
 
